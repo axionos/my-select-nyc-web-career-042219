14da6e2325762b1bbce9eaedd9cd063526f9c1dd
SOYEONG OH <axionoso@gmail.com>
2019-03-22 20:05:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,3 @@
 def my_select(collection)
-  counter = 0
-  new_array = []
-  while counter < collection.length
-    if yield(collection[counter]) == true
-      new_array << collection[counter]
-    end
-    counter += 1
-  end
-  new_array
+
 end
